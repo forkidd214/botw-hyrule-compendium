fd15037347ad434bc54095ff83e93bc17f95a303
jasonfzou <forkidd214@gmail.com>
2023-12-21 17:30:15 +1030

    test(Modal.test.tsx): update unit tests for Modal component

--- src/app/components/Modal/Modal.test.tsx
+++ src/app/components/Modal/Modal.test.tsx
@@ -1,5 +1,5 @@
 import userEvent from '@testing-library/user-event'
-import { screen, render } from '@testing-library/react'
+import { screen, within, render, act, cleanup } from '@testing-library/react'
 import Modal from './Modal'
 
 describe('Modal', () => {
@@ -14,7 +14,6 @@ describe('Modal', () => {
   }
 
   beforeAll(() => {
-    HTMLDialogElement.prototype.show = vi.fn()
     HTMLDialogElement.prototype.showModal = vi.fn()
     HTMLDialogElement.prototype.close = vi.fn()
     HTMLDialogElement.prototype.getBoundingClientRect = vi.fn(() => ({
@@ -30,37 +29,58 @@ describe('Modal', () => {
     }))
   })
 
-  const handleClose = vi.fn()
-  beforeEach(() => {
-    render(
-      <Modal isOpen={false} onClose={handleClose}>
-        <div>modal content</div>
-      </Modal>
-    )
+  afterEach(() => {
+    vi.clearAllMocks() // otherwise, mock function calling times accumulate
   })
 
-  it('renders its children', () => {
-    const modalContent = screen.getByText(/modal content/i)
+  const renderModal = ({
+    isOpen = false,
+    handleClose = vi.fn(),
+  }: {
+    isOpen?: boolean
+    handleClose?: () => void
+  } = {}) => {
+    return {
+      ...render(
+        <Modal isOpen={isOpen} onClose={handleClose}>
+          <div>modal content</div>
+        </Modal>
+      ),
+      handleClose,
+      modal: screen.getByRole('dialog', { hidden: true }) as HTMLDialogElement,
+      modalContent: within(
+        screen.getByRole('dialog', { hidden: true })
+      ).getByText(/modal content/i),
+    }
+  }
+
+  it('hides its children when closed', () => {
+    const { modal, modalContent } = renderModal()
     expect(modalContent).toBeInTheDocument()
+    expect(modal.showModal).toBeCalledTimes(0)
+    expect(modal.close).toBeCalledTimes(1)
   })
 
-  it('keeps open on inside click', async () => {
+  it('shows its children when opend', () => {
+    const { modal, modalContent } = renderModal({ isOpen: true })
+    expect(modalContent).toBeInTheDocument()
+    expect(modal.showModal).toBeCalledTimes(1)
+    expect(modal.close).toBeCalledTimes(0)
+  })
+
+  it('calls onClose callback only on outside click', async () => {
     // Arrange
     const user = userEvent.setup()
+    const { modal, handleClose } = renderModal({ isOpen: true })
     // Act
     await user.pointer({ coords: INSIDE_DIALOG })
-    await user.click(screen.getByRole('dialog', { hidden: true }))
+    await user.click(modal)
     // Assert
     expect(handleClose).toBeCalledTimes(0)
-  })
-
-  it('closes on outside click', async () => {
-    // Arrange
-    const user = userEvent.setup()
     // Act
     await user.pointer({ coords: OUTSIDE_DIALOG })
-    await user.click(screen.getByRole('dialog', { hidden: true }))
+    await user.click(modal)
     // Assert
-    expect(handleClose).toBeCalledTimes(1)
+    expect(modal.showModal).toBeCalledTimes(1)
   })
 })
